dea166998f81928c77d0abf4c2e7203e6b9c6332
janiapurva <jani.apurva@yahoo.com>
2021-05-02 19:32:06 -0400

    basic understanding of typescript

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
-function add(n1,n2){
-  return n1 + n2;
+// define input data by : next to argument
+function add(n1:number,n2:number,showResult:boolean,phrase:string){
+  showResult?console.log(phrase + n1 + n2) : n1 +n2
+  
 }
+

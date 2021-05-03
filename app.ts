a83ede531ccaa0c0cbe4e1f1f7ee767011fd7aab
janiapurva <jani.apurva@yahoo.com>
2021-05-02 20:04:56 -0400

    Introduction  to array data type

--- app.ts
+++ app.ts
@@ -1,7 +1,17 @@
-
 //object type intorduction
-const person = {
-  name : 'Apurva',
-  age : 29
-}
+//define obejct by colon and {} which define as person as a object then you can define inside value as key value type.you can view all data type by just hovering over person object
 
+const person = {
+  name: "Apurva",
+  age: 29,
+  //array data type
+  hobbies: ["Sports", "Cooking"],
+};
+
+console.log(person);
+
+// you can intialize variable with defiening value and data type for array
+let favouriteActivities: string[];
+// assigning value to array .you get the error if you add mixed data type you can mixed data by any 
+// let favouriteActivities: any[];
+favouriteActivities = ['Sports']

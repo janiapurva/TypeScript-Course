10a827a3f909d6412ce108a9f78818160163554f
janiapurva <jani.apurva@yahoo.com>
2021-05-04 20:38:02 -0400

    learn type alias of data

--- app.ts
+++ app.ts
@@ -29,3 +29,23 @@ function combination(
 
   return result;
 }
+
+//type alias
+// define by word type and variable
+// you can store expected data type and reuable for programmme
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as- text'
+
+function alias(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  (typeof input1 === "number" && typeof input2 === "number") ||
+  resultConversion === "as-number"
+    ? (result = +input1 + +input2)
+    : (result = input1.toString() + input2.toString);
+
+  return result;
+}

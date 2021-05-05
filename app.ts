79c29d1ce735737d8af4f8ab40d3dcc1c819e92f
janiapurva <jani.apurva@yahoo.com>
2021-05-04 20:30:03 -0400

    learn literal type of data

--- app.ts
+++ app.ts
@@ -1,9 +1,30 @@
 //Union type
 //nay where in function where function parameter accept two diffrent kind of data type then use Union data type
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: string
+) {
   let result;
-  typeof input1 === "number" && typeof input2 === "number"
-    ? (result = input1 + input2)
+  (typeof input1 === "number" && typeof input2 === "number") ||
+  resultConversion === "as-number"
+    ? (result = +input1 + +input2)
+    : (result = input1.toString() + input2.toString);
+
+  return result;
+}
+
+// literal type basically defining argument directly hardcoded value
+
+function combination(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
+  let result;
+  (typeof input1 === "number" && typeof input2 === "number") ||
+  resultConversion === "as-number"
+    ? (result = +input1 + +input2)
     : (result = input1.toString() + input2.toString);
 
   return result;
